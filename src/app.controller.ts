1a36c5cc4ebfbd142cf07b383a2e7ec5e878af98
minhquang2k4 <minhwang1708@gmail.com>
2024-12-07 22:25:54 +0700

    MVC

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get() // tượng trưng cho phương thức GET của API
+  @Render('home')
+  getHello() {
+    // return 'this.appService.getHello()';
+    const msg = this.appService.getHello();
+
+    return {
+      message: msg,
+    };
   }
 }
